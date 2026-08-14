0bdc6002475d0216d7769368ca2ccaf1a40774b8
monim <monimelqoraychy@gmail.com>
2026-08-14 20:27:43 +0100

    Ajout de l API publique de contact

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "accounts",
     "gyms",
     "members",
+    "contacts",
 ]
 
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
+    path("api/contacts/", include("contacts.urls")),
     path("api/", include("members.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
